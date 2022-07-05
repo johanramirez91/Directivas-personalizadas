ab5d4177935633f76ed5407b282534b36899df2a
johan.ramirez <johan.ramirez@sofka.com.co>
2022-07-05 14:54:32 -0500

    Add: single field reactive form

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styleUrls: ['./agregar.component.scss']
+  styleUrls: ['./agregar.component.scss'],
 })
 export class AgregarComponent implements OnInit {
+  formulario: FormGroup = this.fb.group({
+    nombre: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validarCampo(campo: string): boolean {
+    return (
+      (this.formulario.get(campo)?.invalid || false) &&
+      (this.formulario.get(campo)?.touched || false)
+    );
   }
-
 }
--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AgregarComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductosRoutingModule
-  ]
+  declarations: [AgregarComponent],
+  imports: [CommonModule, ProductosRoutingModule, ReactiveFormsModule],
 })
-export class ProductosModule { }
+export class ProductosModule {}
